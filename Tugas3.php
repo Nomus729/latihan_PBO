2fd2c6682ef3f4f16d251e396df6ab68b1660c15
Nomus729 <skyh1023@gmail.com>
2025-09-03 16:41:26 +0700

    Pertemuan3 final

--- Tugas3.php
+++ Tugas3.php
@@ -1,77 +1,99 @@
 <?php
 
-Class PembayaranMarket{
+class PembayaranMarket {
     var $kartu;
     var $total;
     var $harga;
     var $diskon;
-    var $pembayaran;
 
-    function punyaKartu($x){            
-        $this->kartu=$x;
-        switch ($x){
-            case "ya":
-                $x = 1;
-                break;
-            case "tidak":
-                $x = 0;
-                break;
-        }
-    
-        switch ($x){
-        case 1:
-            if ($this->harga > 500000) $this->diskon = 50000;
-            elseif ($this->harga > 100000) $this->diskon = 15000;
-            else $this->diskon=0;
-            break;
-        case 0:
-            if ($this->harga>100000) $this->diskon = 5000;
-            else $this->diskon=0;
-            break;
-       }
-        return $this->kartu; 
+    function setHarga($hargabeli) {
+        $this->harga = $hargabeli;
     }
 
+    function setKartu($status) {
+        if ($status == "ya") {
+            $this->kartu = $status;
+        } else {
+            $this->kartu = "tidak";
+        }
+    }
 
-    function hargaAkhir(){
-        $this->total = $this->harga - $this->diskon; 
+    function getHarga() {
+        return $this->harga;
+    }
+
+    function getKartu() {
+        return $this->kartu;
+    }
+
+    function getDiskon() {
+        switch ($this->kartu) {
+            case "ya":
+                if ($this->harga > 500000) {
+                    $this->diskon = 50000;
+                } elseif ($this->harga > 100000) {
+                    $this->diskon = 15000;
+                } else {
+                    $this->diskon = 0;
+                }
+                break;
+            case "tidak":
+                if ($this->harga > 100000) {
+                    $this->diskon = 5000;
+                } else {
+                    $this->diskon = 0;
+                }
+                break;
+            default:
+                $this->diskon = 0;
+                break;
+        }
+        return $this->diskon;
+    }
+
+    function getTotalBayar() {
+        $this->total = $this->harga - $this->getDiskon();
         return $this->total;
     }
 }
 
-$pembelian1 = new PembayaranMarket;
-$pembelian1->harga = 200000;
+$pembelian1 = new PembayaranMarket();
+$pembelian1->setHarga(200000);
+$pembelian1->setKartu("ya");
 
-
-echo "Apakah ada kartu member : ".$pembelian1->punyaKartu("ya"). "\n <br>";
-echo "Total belanjaan : ". $pembelian1->harga. "\n <br>";
-echo "Diskon: Rp ". $pembelian1->diskon. "\n <br>";
-echo "Total Bayar: Rp ". $pembelian1->hargaAkhir()."\n <br>";
-
-$pembelian2 = new PembayaranMarket;
-$pembelian2->harga = 570000;
-
+echo "Apakah ada kartu member : " . $pembelian1->getKartu() . "\n <br>";
+echo "Total belanjaan : " . $pembelian1->getHarga() . "\n <br>";
+echo "Diskon: Rp " . $pembelian1->getDiskon() . "\n <br>";
+echo "Total Bayar: Rp " . $pembelian1->getTotalBayar() . "\n <br>";
 echo "<hr>";
-echo "Apakah ada kartu member : ".$pembelian2->punyaKartu("ya"). "\n <br>";
-echo "Total belanjaan : ". $pembelian2->harga. "\n <br>";
-echo "Diskon: Rp ". $pembelian2->diskon. "\n <br>";
-echo "Total Bayar: Rp ". $pembelian2->hargaAkhir()."\n <br>";
 
-$pembelian3 = new PembayaranMarket;
-$pembelian3->harga = 120000;
+$pembelian2 = new PembayaranMarket();
+$pembelian2->setHarga(570000);
+$pembelian2->setKartu("ya");
 
+echo "Apakah ada kartu member : ".$pembelian2->getKartu() . "\n <br>";
+echo "Total belanjaan : " .$pembelian2->getHarga() . "\n <br>";
+echo "Diskon: Rp ".$pembelian2->getDiskon(). "\n <br>";
+echo "Total Bayar: Rp " .$pembelian2->getTotalBayar() . "\n <br>";
 echo "<hr>";
-echo "Apakah ada kartu member : ".$pembelian3->punyaKartu("tidak"). "\n <br>";
-echo "Total belanjaan : ". $pembelian3->harga. "\n <br>";
-echo "Diskon: Rp ". $pembelian3->diskon. "\n <br>";
-echo "Total Bayar: Rp ". $pembelian3->hargaAkhir()."\n <br>";
 
-$pembelian4 = new PembayaranMarket;
-$pembelian4->harga = 90000;
+$pembelian3 = new PembayaranMarket();
+$pembelian3->setHarga(120000);
+$pembelian3->setKartu("tidak");
 
+echo "Apakah ada kartu member : " . $pembelian3->getKartu() . "\n <br>";
+echo "Total belanjaan : " . $pembelian3->getHarga(). "\n <br>";
+echo "Diskon: Rp " . $pembelian3->getDiskon() . "\n <br>";
+echo "Total Bayar: Rp " . $pembelian3->getTotalBayar(). "\n <br>";
 echo "<hr>";
-echo "Apakah ada kartu member : ".$pembelian4->punyaKartu("tidak"). "\n <br>";
-echo "Total belanjaan : ". $pembelian4->harga. "\n <br>";
-echo "Diskon: Rp ". $pembelian4->diskon. "\n <br>";
-echo "Total Bayar: Rp ". $pembelian4->hargaAkhir()."\n <br>";
+
+$pembelian4 = new PembayaranMarket();
+$pembelian4->setHarga(90000);
+$pembelian4->setKartu("tidak");
+
+echo "Apakah ada kartu member : " . $pembelian4->getKartu() . "\n <br>";
+echo "Total belanjaan : " . $pembelian4->getHarga() . "\n <br>";
+echo "Diskon: Rp " . $pembelian4->getDiskon() . "\n <br>";
+echo "Total Bayar: Rp " . $pembelian4->getTotalBayar() . "\n <br>";
+
 ?>
