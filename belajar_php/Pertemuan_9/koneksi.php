8e33bb8e96ff4927f48ae9057fc0f841cc848233
Nomus729 <skyh1023@gmail.com>
2025-10-23 05:02:13 +0700

    Pertemuan 9

---
+++ belajar_php/Pertemuan_9/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "67534";
+$database = "si_gudang";
+
+// isi nama host, username mysql, dan password mysql anda
+$koneksi = mysqli_connect($host,$username,$password,$database);
+
+
+
+?>
